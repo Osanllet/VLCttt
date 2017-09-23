f16fc8fa190e2fe1924cf6d770f7d2a24848d36e
Omar <osanllet@gmail.com>
2017-09-23 12:17:03 -0500

    Primera aproximación al entramado
    Se agregó un módulo que almacena dato a dato recibido.

--- UART_TX.vhd
+++ UART_TX.vhd
@@ -36,11 +36,13 @@ use ieee.numeric_std.all;
  
 entity UART_RX is
   generic (
-    g_CLKS_PER_BIT : integer := 5208     -- Needs to be set correctly
+    g_CLKS_PER_BIT : integer := 5208;     -- Needs to be set correctly
+	 DATA_WIDTH	:	INTEGER := 64
     );
   port (
     i_Clk       : in  std_logic;
     i_RX_Serial : in  std_logic;
+	 i_Reset		 : in	 std_logic;
    -- o_RX_DV     : out std_logic;
     o_RX_Byte   : out std_logic_vector(7 downto 0)
     );
@@ -49,6 +51,9 @@ end UART_RX;
  
 architecture rtl of UART_RX is
  
+  -- ******************** CONSTANTES ******************** 
+  CONSTANT long_t		:	integer := DATA_WIDTH;
+  
   type t_SM_State is (s_Idle, s_RX_Start_Bit, s_RX_Data_Bits,
                      s_RX_Stop_Bit, s_Cleanup);
   signal r_SM_Act_State : t_SM_State := s_Idle;
@@ -59,7 +64,16 @@ architecture rtl of UART_RX is
   signal r_Clk_Count : integer range 0 to g_CLKS_PER_BIT-1 := 0;
   signal r_Bit_Index : integer range 0 to 7 := 0;  -- 8 Bits Total
   signal r_RX_Byte   : std_logic_vector(7 downto 0) := (others => '0');
- -- signal r_RX_DV     : std_logic := '0';
+  signal r_RX_DV     : std_logic := '0';
+ 
+ -- ******************** ENTRAMADO MS ********************
+	signal Data_in		:	unsigned(long_t DOWNTO 0):= to_unsigned(11184642, DATA_WIDTH+1);--(others => '0'); -- TRAMA A GUARDAR EN LA MEMORIA
+	signal Data_out	:	unsigned(long_t DOWNTO 0):=(others => '0');
+	TYPE estados is (espera, agrega, recorre, conteo);
+	signal est_actual	: 	estados:= espera;
+	signal contador  	:	NATURAL RANGE 0 TO 255 := 0;
+	signal data		:	std_logic_vector(7 downto 0) := (others => '0');
+	SIGNAL crcm			: STD_LOGIC:='0';
    
 begin
  
@@ -97,7 +111,7 @@ begin
         when s_RX_Start_Bit =>
           if r_Clk_Count = (g_CLKS_PER_BIT-1)/2 then
             if r_RX_Data = '0' then
-              r_Clk_Count <= 0;  -- reset counter since we found the middle
+              r_Clk_Count <= 0;  -- i_Reset counter since we found the middle
               r_SM_Act_State   <= s_RX_Data_Bits;
             else
               r_SM_Act_State   <= s_Idle;
@@ -135,17 +149,16 @@ begin
             r_Clk_Count <= r_Clk_Count + 1;
             r_SM_Act_State   <= s_RX_Stop_Bit;
           else
-            --r_RX_DV     <= '1';
+            r_RX_DV     <= '1';
             r_Clk_Count <= 0;
             r_SM_Act_State   <= s_Cleanup;
+				data <= r_RX_Byte;
           end if;
- 
-                   
+   
         -- Stay here 1 clock
         when s_Cleanup =>
           r_SM_Act_State <= s_Idle;
-         -- r_RX_DV   <= '0';
- 
+          r_RX_DV   <= '0';
              
         when others =>
           r_SM_Act_State <= s_Idle;
@@ -153,8 +166,48 @@ begin
       end case;
     end if;
   end process p_UART_RX;
- 
+  
   --o_RX_DV   <= r_RX_DV;
-  o_RX_Byte <= r_RX_Byte;
+  --o_RX_Byte <= r_RX_Byte;
    
+p_DATA_FRAMING : process (i_Reset, i_Clk, r_RX_DV) 
+	begin
+		if i_Reset = '1' then
+			est_actual <= espera;
+			Data_in <= (others => '0');
+		elsif rising_edge(i_Clk) then
+			case est_actual is
+				when espera =>
+					if r_RX_DV = '1' then
+						contador <= contador + 1;
+						est_actual <= conteo;
+					else
+						est_actual <= espera;
+					end if;
+					crcm <= '0';
+				when conteo =>
+					if contador = 1 then
+						Data_in <= Data_in rol 8;
+					elsif contador = 5 then
+						crcm <= '1';
+						Data_in <= to_unsigned(11184642, Data_in'length);
+						contador <= 0;
+					else
+						crcm <= '0';
+					end if;
+					--Data_in <= Data_out;
+					est_actual <= agrega;
+				when agrega =>
+					Data_in (7 downto 0) <= unsigned(data);
+					est_actual <= recorre;
+				when recorre => 
+					Data_in <= Data_in rol 8;
+					Data_out <= Data_in rol 8;
+					est_actual <= espera;
+			end case;
+		end if;
+	end process;
+	
+	o_RX_Byte <=  std_logic_vector(Data_out (15 downto 8));
+	
 end rtl;
